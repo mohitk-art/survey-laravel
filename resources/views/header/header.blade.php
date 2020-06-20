f7a56b3443232330dbee08621e14454aba3e47de
mohitk-art <mkjcsoftwaresolution@gmail.com>
2020-06-20 14:34:37 +0530

    fd

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,27 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark main-header">
-    <div class="container">
-  <a class="navbar-brand" href="#">
-      <img src="img/apricotlogo.png"/>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#pricing">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="companylogin">Employer log in</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="employeelogin">Employee log in</a>
-      </li>
-      
-    </ul>
-  </div>
-</div>
-</nav>
+        <div class="container">
+            <!-- <a class="navbar-brand" href="#">
+                <img src="img/apricotlogo.png" />
+            </a> -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="company_login">Manage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="employee_login">Employee log in</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
